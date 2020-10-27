ad79cba8a6037fa2f23eebf43d1ec49a1b34fc40
madalinalzr <72612476+madalinalzr@users.noreply.github.com>
2020-10-27 17:13:21 +0200

    instance lifecycle

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -1,11 +1,15 @@
 package guru.springframework;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class GreetingTest {
 
     Greating greting;
+
+    @BeforeAll
+    public static void beforeClass(){
+        System.out.println("Before - I'm only called once!");
+    }
 
     @BeforeEach
     void setUp() {
@@ -21,5 +25,20 @@ public class GreetingTest {
     @Test
     void helloWorld1(){
         System.out.println(greting.helloWorld("Tom"));
+    }
+
+    @Test
+    void helloWorld2(){
+        System.out.println(greting.helloWorld("Jerry"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("After each...");
+    }
+
+    @AfterAll
+    public static void afterClass(){
+        System.out.println("After - O am called once!");
     }
 }
